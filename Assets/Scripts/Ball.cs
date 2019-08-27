f5802e5a734ec89427f6d91998a0cd302eb4c9fc
Pandakaebi <panda@pandakaebi.me>
2019-08-27 22:41:44 +1000

    Base pong game.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private float speed = 30;
+
+    float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketHeight)
+    {
+        return (ballPos.y - racketPos.y) / racketHeight;
+    }
+
+    void Start()
+    {
+        // Initial velocity
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "RacketLeft")
+        {
+            // Calculate hit factor
+            float y = hitFactor(transform.position, collision.transform.position, collision.collider.bounds.size.y);
+            // Calculate direction, make lenght=1 via .normalized
+            Vector2 dir = new Vector2(1, y).normalized;
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+        if (collision.gameObject.name == "RacketRight")
+        {
+            // Calculate hit factor
+            float y = hitFactor(transform.position, collision.transform.position, collision.collider.bounds.size.y);
+            // Calculate direction, make lenght=1 via .normalized
+            Vector2 dir = new Vector2(-1, y).normalized;
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+    }
+}
---
+++ Assets/Scripts/MoveRacket.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRacket : MonoBehaviour
+{
+    [SerializeField] private float speed = 30;
+    [SerializeField] private string axis = "Vertical";
+
+    private void FixedUpdate()
+    {
+        float v = Input.GetAxisRaw(axis);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
+    }
+}
