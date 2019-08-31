340cde8547fa40ec20489e3e7d55a57866c34242
Pandakaebi <panda@pandakaebi.me>
2019-08-31 13:13:12 +1000

    Changed goal sizes and changed to collision

--- Assets/Scripts/GoalArea.cs
+++ Assets/Scripts/GoalArea.cs
@@ -19,7 +19,7 @@ public class GoalArea : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         gameStateManager.AddScore(playerType);
     }
